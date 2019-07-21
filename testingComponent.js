091e34e27c5b40c8a4df9679508e67ac27a076d6
nghianguyen979 <nghianguyen979@gmail.com>
2019-07-21 17:35:55 +0700

    Update testingComponent.js

--- testingComponent.js
+++ testingComponent.js
@@ -64,14 +64,13 @@ function forgotPass() {
   }
 }
 
-var fallbackToStore = function() {
-  window.location.replace('market://details?id=vn.com.vng.zalopay');
-};
-var openApp = function() {
-  window.location.replace('launhzlp://');
-};
+// var fallbackToStore = function() {
+//   window.location.replace('market://details?id=vn.com.vng.zalopay');
+// };
+// var openApp = function() {
+//   window.location.replace('launhzlp://');
+// };
        
 function triggerAppOpen() {
-  openApp();
-  setTimeout(fallbackToStore, 250);
+   window.location = "https://go.zalopay.vn/f/launch"
 };
